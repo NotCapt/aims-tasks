0e9b82a1f0ecb5bab127806e689fcaeaa41c0e89
NotCapt <faaizhussain006@gmail.com>
2025-10-21 19:31:23 +0530

    new file:   imputation.py

---
+++ imputation.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+
+dict1 = {
+    'city' : ['Noida', 'Delhi', 'Pune', 'Ghaziabad', 'Mumbai', 'Gurgaon'],
+    'Price' : [12e6, 1e7, 9e6, 5e6, 15e6, np.nan],
+    'Bedrooms' : [2, 2, 3, 3, 2, 2]
+}
+
+df = pd.DataFrame(dict1)
+#print(df)
+
+# replacing gurgaon's price with the mean of other values
+df.Price[5] = df.Price.mean()
+#print(df.Price[5])
+
+print(df)
